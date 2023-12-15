96531d68552ae02103aa7e453241519986b19e97
rushabhvg <rushabhvg@gmail.com>
2023-12-15 18:17:47 +0530

    API for creating a new book.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,43 @@
 import express from "express";    // import express framework   for Node.js
 import { PORT, mongoDBURL } from "./config.js"; // import PORT and mongoDBURL from config.js
 import mongoose from "mongoose";    // import mongoose  ODM for MongoDB
+import { Book } from "./models/bookModel.js";   // import Book model from bookModel.js 
 
 const app = express();
+
+// Middleware to parse the body of the request
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to the BookStoreProj!');
+});
+
+// Route to Save a new book to the database
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title || 
+            !request.body.author || 
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Required field(s) missing!"
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 // app.listen(PORT, () => {
