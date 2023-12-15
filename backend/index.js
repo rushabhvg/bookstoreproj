9e91dd4cbfaaf650519dfd46ad958e6544f2167f
rushabhvg <rushabhvg@gmail.com>
2023-12-15 17:46:33 +0530

    Connected online MongoDB to App.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express from "express";    // import express framework   for Node.js
+import { PORT, mongoDBURL } from "./config.js"; // import PORT and mongoDBURL from config.js
+import mongoose from "mongoose";    // import mongoose  ODM for MongoDB
 
 const app = express();
 
@@ -8,6 +9,21 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to the BookStoreProj!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        // if all is ok we will be here
+        console.log("MongoDB connected to the App");
+        // start the server only if connected to the DB
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+          });
+    })
+    .catch((error) => {
+        // if error we will be here
+        console.log(error);
+    });
