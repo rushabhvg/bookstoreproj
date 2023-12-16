d72175d50c527e65b708e3f24fb50ad6ae663ccc
rushabhvg <rushabhvg@gmail.com>
2023-12-16 10:46:14 +0530

    Show Books Frontend, Backend linked

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateBook from "./pages/CreateBooks";
 import ShowBook from "./pages/ShowBook";
@@ -9,13 +9,13 @@ import DeleteBook from "./pages/DeleteBook";
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/books/create" element={<CreateBook />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
-}
+};
 
 export default App;
